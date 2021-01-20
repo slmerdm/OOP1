5d1b6251f4696642ffecddece05b0c6c9518ec7f
Selim <slmerdm95@gmail.com>
2021-01-20 23:54:18 +0300

    Proje dosyası ekle.

---
+++ Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Selim", "Murat","Hüseyin","Mualla" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5]; // 5elemanlı dizi oluştıu.
+            //isimler[4] = "Sami";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string>() {"Selim", "Murat", "Hüseyin", "Mualla" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Sami");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }   //hangi kategoriye ait
+        public string ProductName { get; set; }  //ürün ismi
+        public double UnitPrice { get; set; }  //ürün fiyatı
+        public int UnitsInStock { get; set; }   //stok miktarı
+
+
+    }
+}
